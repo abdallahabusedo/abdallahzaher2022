a1402db912397c5c6eb7ca07bab2c49e1290a7c6
AbdallahZaher <abdallah.zaher@intellaworld.com>
2022-11-22 12:53:03 +0200

    skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -70,7 +70,9 @@ function Skills() {
       id="skills"
       className={`${styles.Welcome} flex flex-col gap-8 justify-center items-center p-4`}
     >
-      <span className={`${styles.text}`}>Skills</span>
+      <span className={`${styles.text}`}>
+        {"{"} Skills {"}"}
+      </span>
 
       <div
         className={`${styles.glassEffect} grid grid-cols-6 justify-items-center justify-center gap-4 items-center w-2/3 bg-[#290e56] p-4`}
